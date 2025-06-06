5f7a2d27bb07f0b854e0c6a8f49a91439205b9b0
Termux-Ma <arishahmedtushar8@gmail.com>
2025-06-06 16:28:53 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  ownerUsernames: ["MOHAMMADNAYAN"],//Your Username
-  ownerNumber: "+8801615298449",//Your Number
-  admin: ["5920596290", "8179590682"], // your id
+  ownerUsernames: ["MOHAMMADTUSHAR"],//Your Username
+  ownerNumber: "+8801758269324",//Your Number
+  admin: ["7159426600", "7159426600"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "token",//Your Bot Tokens
+  telegramBotToken: "8131966319:AAF_b1WKnR6alAt-Thu0u5vKq8X4CySrg04 Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   po0rt: process.env.PORT || 8053 || 8053, // port
   mess: {
-    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    ownerlink: "https://t.me/bkcyber01",
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
     group: "Sorry, this command can only be used within a group!",
